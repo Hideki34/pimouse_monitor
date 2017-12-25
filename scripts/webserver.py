053e4649d2f2f33ec568acb6b9d10d66a4c1e39c
Hideki Ogura <b1415988@planet.kanazawa-it.ac.jp>
2017-12-25 06:11:34 +0000

    Add files

--- scripts/webserver.py
+++ scripts/webserver.py
@@ -3,7 +3,7 @@ import rospy, os
 import SimpleHTTPServer
 
 def kill():
-	os.system("kill -KILL" + str(os.getpid()))
+    os.system("kill -KILL " + str(os.getpid()))
 
 os.chdir(os.path.dirname(__file__) + "/../contents")
 rospy.init_node("webserver")
